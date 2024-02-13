9960e896314e20e4750a1638ead09706541f5bdc
diptanshumahish <diptanshumahish2016@gmail.com>
2024-02-13 13:16:16 +0530

    added new logo and metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,25 @@ export const metadata: Metadata = {
   title: "Cursed Diary",
   description:
     "Your go to website if you wanna read about some creepy stuff | horror blogs | Real life incidents",
+  openGraph: {
+    title: "Cursed Diary",
+    description:
+      "Your go to website for creepy, horror stuff, be here if you want to read something new, something cool",
+    images:
+      "https://img.playbook.com/NktCs8OODKJS3YFewWmjzpqchieg42AkC0SSv3iW2kU/Z3M6Ly9wbGF5Ym9v/ay1hc3NldHMtcHVi/bGljL2MzODQ1MDMz/LTFhMmYtNGIxZS04/OTAyLTgyZDdkM2Ix/YzliZA",
+    countryName: "IN",
+    siteName: "https://curseddiary.vercel.app",
+  },
+  metadataBase: new URL("https://curseddiary.vercel.app"),
+  publisher: "Cursed Diary",
+  robots: "index,follow",
+  twitter: {
+    title: "Cursed Diary",
+    images:
+      "https://img.playbook.com/NktCs8OODKJS3YFewWmjzpqchieg42AkC0SSv3iW2kU/Z3M6Ly9wbGF5Ym9v/ay1hc3NldHMtcHVi/bGljL2MzODQ1MDMz/LTFhMmYtNGIxZS04/OTAyLTgyZDdkM2Ix/YzliZA",
+    description:
+      "Your go to website for creepy, horror stuff, be here if you want to read something new, something cool",
+  },
 };
 
 export default function RootLayout({
