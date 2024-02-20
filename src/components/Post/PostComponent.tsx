09d38d2dfdf1946f88c4250a2dbfe50ad76d6eb8
diptanshumahish <diptanshumahish2016@gmail.com>
2024-02-20 13:02:17 +0530

    changed post components

--- src/components/Post/PostComponent.tsx
+++ src/components/Post/PostComponent.tsx
@@ -5,6 +5,7 @@ import moment from "moment";
 import Image from "next/image";
 import assets from "../services/assets";
 import { Post } from "../services/post";
+import { ArrowRightCircle } from "lucide-react";
 
 interface Props {
   post: Post;
@@ -22,7 +23,14 @@ export default async function PostComponent({ post, postType }: Props) {
       data-genre={genres}
       data-catagory={postType}
     >
-      <div className="border p-2 overflow-hidden flex  hover:bg-[#ebc99a10] text-white  transition-colors justify-between border-white border-opacity-20 rounded-sm">
+      <div className="border overflow-hidden flex flex-col  hover:bg-[#ebc99a10] text-white  transition-colors justify-between border-white border-opacity-20 rounded-sm">
+        <div className="flex justify-between items-center px-4 py-2 border-b border-white border-opacity-40 py-">
+          <span>{moment(post.created_time).format("D MMM,YYYY")}</span>
+          <span className="flex items-center gap-2">
+            Read <ArrowRightCircle size={16} />
+          </span>
+        </div>
+        <h3 className="text-lg line-clamp-2 px-4 py-2 ">{post.title}</h3>
         <Image
           unoptimized
           src={post.cover ?? assets.blur_img}
@@ -31,15 +39,13 @@ export default async function PostComponent({ post, postType }: Props) {
           blurDataURL={assets.blur_img}
           placeholder="blur"
           width={200}
-          className="w-[40%] h-auto aspect-[9/16] bg-theme-bg object-cover border border-white border-opacity-40 rounded-sm"
+          className="w-full h-auto aspect-[16/9] bg-theme-bg object-cover border-y border-white border-opacity-40 rounded-sm"
         />
-        <div className="w-[55%] h-full flex flex-col gap-2">
-          <h3 className="text-lg line-clamp-2 ">{post.title}</h3>
-          <hr className="opacity-40" />
+        <div className=" h-full flex flex-col gap-2 px-4 py-2">
           <span className="h-[45%] opacity-50 text-sm w-full text-ellipsis overflow-hidden line-clamp-4">
             {post.desc}
           </span>
-          <span>{moment(post.created_time).format("D MMM,YYYY")}</span>
+
           <span className="flex flex-wrap gap-1 text-sm">
             {post.tags!.slice(0, 2).map((post, idx) => {
               return (
